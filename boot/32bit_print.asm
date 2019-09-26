8fb34f0d71b1cc5b8f641b2a41ba7d8c9a44e0be
yadavsunny05 <45046442+yadavsunny05@users.noreply.github.com>
2019-09-27 00:41:46 +0530

    Update 32bit_print.asm

--- boot/32bit_print.asm
+++ boot/32bit_print.asm
@@ -1,8 +1,6 @@
 [bits 32] 
-
-
-VIDEO_MEMORY equ 0xb8000
 COLOR equ 0x4d 
+VIDEO_MEMORY equ 0xb8000
 
 print_string_pm:
     pusha
@@ -11,14 +9,11 @@ print_string_pm:
 print_string_pm_loop:
     mov al, [ebx] 
     mov ah, COLOR
-
     cmp al, 0 
     je print_string_pm_done
-
     mov [edx], ax 
     add ebx, 1 
     add edx, 2 
-
     jmp print_string_pm_loop
 
 print_string_pm_done:
